6905853a1ed5a72cac765c9325b2e2d8be15ae20
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2025-04-14 18:35:35 +0700

    chatting

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,34 @@
 <script setup lang="ts">
+import { nextTick, ref, useTemplateRef } from 'vue'
+
+const messageInput = useTemplateRef('message-input');
+const newMessage = ref('')
+
+const sendMessage = () => {
+  if (newMessage.value.trim() === '') {
+    newMessage.value = ''
+    return
+  }
+
+  console.log(`Kirim Pesan: ${newMessage.value}`)
+
+  newMessage.value = ''
+  nextTick(() => {
+    messageInput.value?.focus()
+  })
+}
 </script>
 
 <template>
   <main>
-    Halaman Utama
+    <div>
+      isi chat
+    </div>
+    <div>
+      <form @submit.prevent="sendMessage">
+        <input type="text" v-model="newMessage" ref="message-input" placeholder="Ketik pesan..." />
+        <input type="submit" value="Kirim">
+      </form>
+    </div>
   </main>
 </template>
