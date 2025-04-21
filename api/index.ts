cd2c1f37e306de7313aa163133ea4a6958aaea27
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2025-04-21 18:17:37 +0700

    chats api

--- api/index.ts
+++ api/index.ts
@@ -2,16 +2,16 @@
 
 interface Env {
   ASSETS: Fetcher;
+  CHAT_DB: D1Database;
 }
 
 export default {
-  fetch(request, env) {
+  async fetch(request, env) {
     const url = new URL(request.url);
 
-    if (url.pathname.startsWith("/api/")) {
-      return Response.json({
-        name: "Cloudflare",
-      });
+    if (url.pathname.startsWith("/api/chats")) {
+      const { results } = await env.CHAT_DB.prepare("SELECT * FROM chats").all();
+      return Response.json(results);
     }
 
     return env.ASSETS.fetch(request);
